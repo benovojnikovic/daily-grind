bec9ae852a7af5f21edb2d608303c43f3307d2d1
benovojnikovic <vojnikbeno1@gmail.com>
2022-07-11 10:23:49 -0700

    all coffee listed

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -37,18 +37,60 @@ let myDate = new Date();
 let myDay = myDate.getDay();
 let coffee = "";
 
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate querystring parameters
+const urlParams = new URLSearchParams(queryString);
+
+if(urlParams.has("day")){  // data available? show data from url
+    myDay = urlParams.get("day");
+
+
+}
+
+myDay = parseInt(myDay);
+
+
 switch(myDay){
 
     case 0:
         today = "Sunday";
+        coffee = {
+        color: "gray",
+            name: "Cold Brew",
+            pic: "images/cold-brew.jpg",
+            alt: "A picture of Cold Brew",
+            day: "Sunday",
+            desc: `The Cold Brew is godly!`
+}
     break;
 
     case 1:
         today = "Monday"
+        coffee = {
+            color: "black",
+                name: "Drip",
+                pic: "images/drip.jpg",
+                alt: "A picture of Drip Coffee",
+                day: "Monday",
+                desc: `The Drip is the greatest menu item ever!`
+    }
     break;
 
     case 2:
-        today = "Tuesday";
+        today = "Tuesday",
+        coffee = {
+        color: "green",
+            name: "Caramel Latte",
+            pic: "images/caramel-latte.jpg",
+            alt: "A picture of Caramel Latte",
+            day: "Tuesday",
+            desc: `I cannot live without Caramel Latte's`
+        }
     break;
 
     case 3:
@@ -77,10 +119,26 @@ switch(myDay){
 
     case 5:
         today = "Friday"
+        coffee = {
+            color: "Orange",
+            name: "Pumpkin Spice Latte",
+            pic: "images/pumpkin-spice-latte.jpg",
+            alt: "A picture of a Pumpkin Spice Latte",
+            day: "Friday",
+            desc: `October is the best month because of Pumpkin Spice Latte's!`
+        };  
     break;
 
     case 6:
         today = "Saturday"
+        coffee = {
+                color: "cyan",
+                name: "Frappaccino",
+                pic: "images/frappaccino.jpg",
+                alt: "A picture Frappaccino",
+                day: "Saturday",
+                desc: `Sweet, sweet frappe yummy so good I love!`
+    }
     break;
 
 
@@ -92,5 +150,6 @@ switch(myDay){
 // let coffeeData = coffeeTemplate(coffee);
 console.log(coffee);
 document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+document.querySelector("html").style.backgroundColor = coffee.color;
 // load coffee data to page
 // alert(coffeeData);
